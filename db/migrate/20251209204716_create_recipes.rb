d7022e93679ca621d054a94068f2201ed59157ec
PEDRO <pedro.araya.labra@gmail.com>
2025-12-09 17:48:34 -0300

    add recipe model

---
+++ db/migrate/20251209204716_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :recipes do |t|
+      t.float :coot_time
+      t.float :prep_time
+      t.float :ratings
+      t.string :title
+      t.string :image
+      t.string :category
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
