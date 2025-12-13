94ee1ffbc10df6defba51a6094279122c65f2a8d
PEDRO <pedro.araya.labra@gmail.com>
2025-12-13 19:15:00 -0300

    add stars displayer helper method

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+  def star_icons(value, from: 0, to: 5)
+    stars = Array.new(to, content_tag(:i, "", class: "bi bi-star"))
+    value_int = value.to_i
+    value_int.times { |index| stars[index] = content_tag(:i, "", class: "bi bi-star-fill") }
+    stars[value_int] = content_tag(:i, "", class: "bi bi-star-half") if value > value_int
+    stars
+  end
 end
