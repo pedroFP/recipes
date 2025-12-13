9a9c2d268d2d85bd6363c0b6576f442902f32812
PEDRO <pedro.araya.labra@gmail.com>
2025-12-13 20:40:57 -0300

    update recipes UI

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,9 +1,23 @@
 module RecipesHelper
-  def star_icons(value, from: 0, to: 5)
+  def display_stars(value, from: 0, to: 5)
     stars = Array.new(to, content_tag(:i, "", class: "bi bi-star"))
     value_int = value.to_i
     value_int.times { |index| stars[index] = content_tag(:i, "", class: "bi bi-star-fill") }
     stars[value_int] = content_tag(:i, "", class: "bi bi-star-half") if value > value_int
-    stars
+    tag.span(title: value) do
+      safe_join stars
+    end
+  end
+
+  def display_time(value_in_minutes)
+    value_in_hours = (value_in_minutes / 60).round(2)
+    message  = value_in_hours > 1 ? "#{value_in_hours} hours" : "#{value_in_minutes} minutes"
+
+    tag.span(title: value_in_hours) do
+      safe_join([
+        message,
+        content_tag(:i, "", class: "mx-2 bi bi-clock")
+      ])
+    end
   end
 end
