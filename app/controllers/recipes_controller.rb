46b46bb45f7f246176f5ffd60c77f11c80994fef
PEDRO <pedro.araya.labra@gmail.com>
2025-12-13 23:43:12 -0300

    update recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,79 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[ show edit update destroy ]
+
+  # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all.limit(20)
   end
+
+  # GET /recipes/1 or /recipes/1.json
+  def show
+  end
+
+  # GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
+
+  # GET /recipes/1/edit
+  def edit
+  end
+
+  # recipe /recipes or /recipes.json
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to @recipe, notice: "recipe was successfully created." }
+        format.json { render :show, status: :created, location: @recipe }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /recipes/1 or /recipes/1.json
+  def update
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to @recipe, notice: "recipe was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /recipes/1 or /recipes/1.json
+  def destroy
+    @recipe.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to recipes_path, notice: "recipe was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def recipe_params
+      params.expect(recipe: [
+        :cook_time,
+        :prep_time,
+        :ratings,
+        :title,
+        :image,
+        :category,
+        :author,
+        ingredients: []
+      ])
+    end
 end
