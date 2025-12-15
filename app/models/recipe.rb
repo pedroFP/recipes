6d092e2ff5d57a5a8a304a854a2e16a8160cbbcf
PEDRO <pedro.araya.labra@gmail.com>
2025-12-14 21:39:17 -0300

    add scope to search ingredients by array

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  scope :search_ingredients, ->(ingredients) { where("ingredients @> ARRAY[?]::varchar[]", ingredients) }
 
   def total_time
     @total_time ||= prep_time + cook_time
