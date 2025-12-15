832fd56c4a6e1319103c438dd41f327f75bed91a
PEDRO <pedro.araya.labra@gmail.com>
2025-12-15 18:08:52 -0300

    removed recipe callback

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,16 +7,7 @@ class Recipe < ApplicationRecord
                       tsvector_column: "ingredients_tsv"
                     }
                   }
-
-  # before_save :update_ingredients_text
-
   def total_time
     @total_time ||= prep_time + cook_time
   end
-
-  private
-
-  # def update_ingredients_text
-  #   self.ingredients_text = self.ingredients.join(",")
-  # scope :search_ingredients, ->(ingredients) { where("ingredients @> ARRAY[?]::varchar[]", ingredients) }
 end
