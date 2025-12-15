803ba26fc311feba8763ac7c1ccad7ba4b05c981
PEDRO <pedro.araya.labra@gmail.com>
2025-12-15 18:38:24 -0300

    condtionalize the submit search button

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,13 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = ["ingredientElement", "ingredients", "ingredientInput"];
+  static targets = [
+    "ingredientElement",
+    "ingredients",
+    "ingredientInput",
+    "submitButton",
+  ];
 
   addIngredient(event) {
     event.preventDefault();
     const ingredient = this.ingredientInputTarget.value;
+    const hasIngredients = this.ingredientInputTargets > 0;
+    const submitButton = this.submitButtonTarget;
 
-    if (ingredient.length == 0) {
+    this.toggleSubmitButton(submitButton, hasIngredients);
+
+    if (hasIngredients) {
       return;
     }
 
@@ -24,7 +33,7 @@ export default class extends Controller {
 
     const badge = document.createElement("link");
     badge.type = "button";
-    badge.className = "badge text-bg-secondary me-2";
+    badge.className = "badge text-bg-secondary me-2 text-decoration-none";
     badge.setAttribute("data-search-target", "badge");
     badge.setAttribute("data-value", ingredient);
     badge.setAttribute("data-action", "click->search#removeIngredient");
@@ -42,5 +51,20 @@ export default class extends Controller {
     );
     ingridientInput[0].remove();
     event.currentTarget.remove();
+
+    const hasIngredients = this.ingredientInputTargets.length > 0;
+    const submitButton = this.submitButtonTarget;
+
+    this.toggleSubmitButton(submitButton, hasIngredients);
+  }
+
+  toggleSubmitButton(submitButton, hasIngredients) {
+    console.log(submitButton);
+    console.log(submitButton.classList);
+    if (hasIngredients) {
+      submitButton.classList.add("d-none");
+    } else {
+      submitButton.classList.remove("d-none");
+    }
   }
 }
