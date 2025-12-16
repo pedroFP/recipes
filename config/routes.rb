b13ecb84af4c2d211b1f27dfd556947abe349b22
PEDRO <pedro.araya.labra@gmail.com>
2025-12-15 23:20:27 -0300

    organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root "recipes#index"
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root "recipes#index"
+
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
