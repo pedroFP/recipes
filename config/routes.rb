b417473bc307e17a30402f75617d9cb64e469f8b
PEDRO <pedro.araya.labra@gmail.com>
2025-12-15 22:56:34 -0300

    generate devise sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root "recipes#index"
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
