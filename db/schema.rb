7d66bd7a24c0479ddba32db8dfb83a9e99a1c542
PEDRO <pedro.araya.labra@gmail.com>
2025-12-16 00:15:26 -0300

    add user to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_16_002726) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_16_031302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,7 +27,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_16_002726) do
     t.tsvector "ingredients_tsv"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["ingredients_tsv"], name: "index_recipes_on_ingredients_tsv", using: :gin
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_16_002726) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "recipes", "users"
 end
